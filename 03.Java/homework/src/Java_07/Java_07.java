03807438fec9348ee47f881d83f6e251efe49009
peanutBana <diao0321@gmail.com>
2022-11-21 11:20:24 +0900

    'java07'

---
+++ 03.Java/homework/src/Java_07/Java_07.java
@@ -0,0 +1,25 @@
+package Java_07;
+
+public class Java_07 {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		//01 ==> 3
+		
+		//02 ==> 2
+		
+		//03 => 1
+		
+		//04 => 4
+		
+		//05 => 4
+		
+		//06 => Child에서 클래스의 name을 super(name)으로 가져와야 한다.
+		Child child = new Child();
+		
+	}
+
+}
